28ee8c5ce595c0097d7a9f085f9a84f0dfb1c5bb
Ian Smith <ian_smith_12a@mymts.net>
2016-02-02 20:27:53 -0600

    Just a simply attempt at a vertex class, shouldn't need one for edges but I'm not at all sure.

--- stuff.py
+++ stuff.py
@@ -1,1 +1,31 @@
-print("Stuff")
+#need a bunch of classes to represent the polyhedron
+
+# each vertex knows the vertex it is connected to
+class Vertex:
+	name = ""
+	#connections = []
+	
+	def __init__(self, vertex):
+		self.name = vertex
+		
+		# it can be assumed that there are edges connecting this vertex to the ones in the list
+		self.connections = []
+	
+	def insertVertex(self, vertex):
+		if vertex:
+			self.connections.append(vertex)
+		print(self.connections)
+			
+	def toString(self):
+		return self.name + ": " + str(self.connections)
+
+x = Vertex("F")
+x.insertVertex("G")
+x.insertVertex("H")
+print(x.toString())
+
+y = Vertex("Q")
+y.insertVertex("R")
+y.insertVertex("S")
+print(x.toString())
+print(y.toString())
