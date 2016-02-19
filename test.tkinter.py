7b6737fb004cc8439c5d8465e992c35fb2fa8b9c
smithi35 <ian_smith_12a@mymts.net>
2016-02-19 13:40:37 -0800

    Can create multiple polygons

--- test.tkinter.py
+++ test.tkinter.py
@@ -5,7 +5,11 @@ top = Tk()
 c = Canvas(top, bg="white", height=600, width=1000)
 
 coord = [100, 100, 100, 500, 600, 200, 300, 200]
-polygon = c.create_polygon(coord, outline='black', fill='red', width=2)
-
+polygon = c.create_polygon(coord, outline='black', fill='blue', width=2)
 c.pack(fill=BOTH, expand=1)
+
+coord = [100, 100, 500, 500, 1000, 600]
+item = c.create_polygon(coord, outline='black', fill='red', width=2)
+c.pack(fill=BOTH, expand=1)
+
 top.mainloop()
